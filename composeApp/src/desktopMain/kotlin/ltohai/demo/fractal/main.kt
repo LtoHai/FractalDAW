74c53262066816a9c781a4727f3830ca467e9c9a
Heisenberg-Werner0 <1526890090@qq.com>
2025-07-02 14:18:42 +0800

    first commit

---
+++ composeApp/src/desktopMain/kotlin/ltohai/demo/fractal/main.kt
@@ -0,0 +1,13 @@
+package ltohai.demo.fractal
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "FractalDAW",
+    ) {
+        App()
+    }
+}
